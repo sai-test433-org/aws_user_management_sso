effff61664b9ed458303fe5e9afcc3200b0e0b76
GsK349 <114642449+RB-SaiKumar@users.noreply.github.com>
2024-07-03 20:51:22 +0530

    single permission set assignement

---
+++ terraform/custom_permission_sets.tf
@@ -0,0 +1,51 @@
+data "aws_ssoadmin_instances" "example" {}
+
+
+#create permission sets
+resource "aws_ssoadmin_permission_set" "ReadOnlyAccess" {
+  instance_arn = tolist(data.aws_ssoadmin_instances.example.arns)[0]
+  name         = "S3ReadFullAccess"
+  description  = "Permission set for S3 read access"
+}
+
+#inline policy for the permission sets
+# Define IAM policies as data sources
+data "aws_iam_policy_document" "s3_read_policy" {
+  statement {
+    sid = "1"
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
+    ]
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+}
+
+#permission set and inline policy attachment
+resource "aws_ssoadmin_permission_set_inline_policy" "s3_read_inline_policy" {
+  inline_policy      = data.aws_iam_policy_document.s3_read_policy.json
+  instance_arn       = tolist(data.aws_ssoadmin_instances.example.arns)[0]
+  permission_set_arn = aws_ssoadmin_permission_set.ReadOnlyAccess.arn
+}
+
+data "aws_ssoadmin_permission_set" "permission_sets" {
+  for_each = { for access in local.account_access : access.permission_set_name => access }
+
+  instance_arn = tolist(data.aws_ssoadmin_instances.example.arns)[0]
+  name         = each.key
+}
+
+resource "aws_ssoadmin_account_assignment" "account_assignments" {
+  for_each = { for access in local.account_access : "${access.group_name}-${access.permission_set_name}-${access.account_id}" => access }
+
+  instance_arn       = tolist(data.aws_ssoadmin_instances.example.arns)[0]
+  permission_set_arn = data.aws_ssoadmin_permission_set.permission_sets[each.value.permission_set_name].arn
+
+  principal_id   = aws_identitystore_group.groups[each.value.group_name].group_id
+  principal_type = "GROUP"
+
+  target_id   = each.value.account_id
+  target_type = "AWS_ACCOUNT"
+}
